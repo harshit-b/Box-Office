90de82149c241ed1f52e16299e211e3c0211ba50
Harshit Bhargava <harshit.bhargava@ttu.edu>
2022-09-30 13:48:09 -0500

    Added Route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
-  return <div>Hi</div>;
+  return (
+    <Switch>
+      <Route exact path="/">
+        This is home page
+      </Route>
+
+      <Route path="/starred">This is starred</Route>
+
+      <Route>NOT FOUND :(</Route>
+    </Switch>
+  );
 }
 
 export default App;
