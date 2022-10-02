fa88c12d0c5c80e59debd2d11b5d03e52345cdf1
Harshit Bhargava <harshit.bhargava@ttu.edu>
2022-10-02 18:21:20 -0500

    TVmaze API, added search, and printed results

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
+import { apiGet } from '../misc/config';
 
 const Home = () => {
-  return <MainPageLayout>This is HOME</MainPageLayout>;
+  const [input, setInput] = useState('');
+  const [results, setResults] = useState(null);
+
+  const onInputChange = ev => {
+    setInput(ev.target.value);
+  };
+
+  const onSearch = () => {
+    // https://api.tvmaze.com/search/shows?q=girls
+
+    apiGet(`/search/shows?q=${input}`).then(result => {
+      setResults(result);
+    });
+  };
+
+  const onKeyDown = ev => {
+    if (ev.keyCode === 13) {
+      onSearch();
+    }
+  };
+
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div>Nothing for you, lmao</div>;
+    }
+
+    if (results && results.length > 0) {
+      return (
+        <div>
+          {results.map(movie => (
+            <div key={movie.show.id}>{movie.show.name}</div>
+          ))}
+        </div>
+      );
+    }
+
+    return null;
+  };
+
+  return (
+    <MainPageLayout>
+      <input
+        type="text"
+        onChange={onInputChange}
+        onKeyDown={onKeyDown}
+        value={input}
+      />
+      <button type="button" onClick={onSearch}>
+        {' '}
+        Search{' '}
+      </button>
+      {renderResults()}
+    </MainPageLayout>
+  );
 };
 
 export default Home;
