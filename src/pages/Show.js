e56e6ce26cdd29b4eb0269b5d68bd6fe70d5de31
Harshit Bhargava <harshit.bhargava@ttu.edu>
2022-10-08 19:28:44 -0500

    Replaced useState with useReducer on show page

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,13 +1,34 @@
 /* eslint-disable */
 
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useState, useEffect, useReducer } from 'react';
+import { NavLink, useParams } from 'react-router-dom';
 import { apiGet } from '../misc/config';
 
 const Show = () => {
-  const [show, setShow] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
+  // const [show, setShow] = useState(null);
+  // const [isLoading, setIsLoading] = useState(true);
+  // const [error, setError] = useState(null);
+
+  const reducer = (prevState, action) => {
+    switch (action.type) {
+      case 'FETCH_SUCCESS': {
+        return { isLoading: false, show: action.show, error: null };
+      }
+
+      case 'FETCH_FAILED': {
+        return { ...prevState, isLoading: false, error: action.error };
+      }
+      default:
+        return prevState;
+    }
+  };
+  const initialState = {
+    show: null,
+    isLoading: true,
+    error: null,
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const { id } = useParams();
 
@@ -17,14 +38,22 @@ const Show = () => {
     apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
       .then(results => {
         if (isMounted) {
-          setShow(results);
-          setIsLoading(false);
+          dispatch({
+            type: 'FETCH_SUCCESS',
+            show: results,
+          });
+          // setShow(results);
+          // setIsLoading(false);
         }
       })
       .catch(err => {
         if (isMounted) {
-          setError(err.message);
-          setIsLoading(false);
+          dispatch({
+            type: 'FETCH_FAILED',
+            error: err,
+          });
+          // setError(err.message);
+          // setIsLoading(false);
         }
       });
 
@@ -33,15 +62,15 @@ const Show = () => {
     };
   }, [id]);
 
-  if (isLoading) {
+  if (state.isLoading) {
     return <div> Data is being loaded </div>;
   }
 
-  if (error) {
+  if (state.error) {
     return <div> {error} </div>;
   }
 
-  console.log(show);
+  console.log(state);
   // apiGet(`/search`)
 
   return <div>This is Show Page</div>;
